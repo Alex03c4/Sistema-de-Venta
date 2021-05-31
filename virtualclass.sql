8d9a266a995022003fd510f8f4c97f4960b84f04
Alex <NaudisGarcia51@gmail.com>
2021-05-31 13:34:12 -0430

    V 0.3.2 Fin de Producto

--- virtualclass.sql
+++ virtualclass.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-05-2021 a las 23:27:54
+-- Tiempo de generación: 31-05-2021 a las 19:30:13
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 7.3.1
 
@@ -35,6 +35,25 @@ CREATE TABLE `images` (
   `img_type` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `images`
+--
+
+INSERT INTO `images` (`id`, `url`, `img_id`, `img_type`) VALUES
+(1, 'naudis.jpg', 1, 1),
+(2, 'Mary.jpg', 1, 3),
+(3, 'Mary.jpg', 2, 3),
+(4, 'Mary.jpg', 3, 3),
+(5, 'Mary.jpg', 4, 3),
+(6, 'Mary.jpg', 5, 3),
+(7, 'Mary.jpg', 6, 3),
+(8, 'Mary.jpg', 7, 3),
+(9, 'Mary.jpg', 8, 3),
+(10, 'Emili.jpg', 10, 3),
+(11, 'Klondy.png', 11, 3),
+(12, 'Klondy.png', 17, 3),
+(13, 'Klondy.png', 18, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -52,7 +71,48 @@ CREATE TABLE `models` (
 
 INSERT INTO `models` (`id`, `url`) VALUES
 (1, 'User'),
-(2, 'Perfil');
+(2, 'Perfil'),
+(3, 'Producto');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `precio` float NOT NULL,
+  `marca` varchar(50) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `descripcion` text NOT NULL,
+  `estatus` int(11) NOT NULL,
+  `id_proveedor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `nombre`, `precio`, `marca`, `stock`, `descripcion`, `estatus`, `id_proveedor`) VALUES
+(1, 'pepe pito dormilos', 2, '', 0, '', 2, 3),
+(2, 'pepito dormilos', 2, '', 0, '', 2, 3),
+(3, 'Mary 3', 20, '20', 20, '<p>20</p>', 2, 3),
+(4, 'Mary 3', 20, '20', 20, '<p>20</p>', 2, 3),
+(5, 'Mary 4', 20, '20', 20, '<p>20</p>', 2, 3),
+(6, 'Mary 4', 20, '20', 20, '<p>20</p>', 2, 3),
+(7, 'Mary 4', 20, '20', 20, '<p>20</p>', 2, 3),
+(8, 'Mary 4', 20, '20', 20, '<p>20</p>', 2, 3),
+(9, 'pepe', 20, '20', 20, '<p>20</p>', 2, 3),
+(10, 'pepe', 40, '2000', 2000, '<p>naudis</p>', 2, 3),
+(11, 'pepito 2 ', 50, '50', 50, '<p>50</p>', 2, 3),
+(12, '2', 0, '', 0, '', 2, 3),
+(14, '3', 0, '', 0, '', 2, 3),
+(15, '1', 0, '', 0, '', 1, 2),
+(16, 'pepe pito dormilos', 2, '', 0, '', 2, 3),
+(17, 'naudis garcia', 50, '50', 50, '<p>50</p>', 1, 3),
+(18, 'lolita', 50, '50', 50, '<p>50</p>', 2, 3);
 
 -- --------------------------------------------------------
 
@@ -70,6 +130,36 @@ CREATE TABLE `profiles` (
   `direccion` varchar(200) NOT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `profiles`
+--
+
+INSERT INTO `profiles` (`id`, `nombre`, `apellido`, `cedula`, `sexo`, `telefono`, `direccion`, `user_id`) VALUES
+(1, 'naudis', 'garcia', '', '', '04245366490', 'la orquidea Calle 6, Barquisimeto 3001, Lara', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores`
+--
+
+CREATE TABLE `proveedores` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `direccion` varchar(200) NOT NULL,
+  `descripcion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `proveedores`
+--
+
+INSERT INTO `proveedores` (`id`, `nombre`, `telefono`, `direccion`, `descripcion`) VALUES
+(1, 'naudis garcia p', '04245366490', 'la orquidea', 'hola mundo como esta'),
+(2, 'Pesi', '0452369548', 'LAlA', ''),
+(3, 'Cocacola', '04245947482', '', '');
 
 -- --------------------------------------------------------
 
@@ -95,6 +185,64 @@ INSERT INTO `roles` (`id`, `nombre`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `taggables`
+--
+
+CREATE TABLE `taggables` (
+  `id` int(11) NOT NULL,
+  `taggable_id` int(11) NOT NULL,
+  `tag_type` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `taggables`
+--
+
+INSERT INTO `taggables` (`id`, `taggable_id`, `tag_type`, `tag_id`) VALUES
+(1, 3, 3, 7),
+(2, 6, 3, 6),
+(3, 7, 3, 6),
+(4, 7, 3, 5),
+(5, 7, 3, 4),
+(6, 7, 3, 3),
+(40, 5, 3, 7),
+(41, 5, 3, 5),
+(96, 16, 3, 7),
+(97, 16, 3, 6),
+(98, 17, 3, 7),
+(99, 18, 3, 7),
+(100, 15, 3, 7),
+(101, 15, 3, 7),
+(102, 4, 3, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tags`
+--
+
+INSERT INTO `tags` (`id`, `nombre`) VALUES
+(1, 'Perro'),
+(2, 'Gato'),
+(3, 'Cochino'),
+(4, 'Loro'),
+(5, 'vaca'),
+(6, 'Pie Gande'),
+(7, 'Aguila');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `users`
 --
 
@@ -103,6 +251,13 @@ CREATE TABLE `users` (
   `email` varchar(150) NOT NULL,
   `pass` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `pass`) VALUES
+(1, 'naudisgarcia51@gmail.com', '$2y$12$HbbJDtiTNt0.NnoSjV1tOudIafgsjXtWq574z8K0k70Vcf1h7/b9m');
 
 -- --------------------------------------------------------
 
@@ -133,6 +288,13 @@ ALTER TABLE `models`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_proveedor` (`id_proveedor`);
+
+--
 -- Indices de la tabla `profiles`
 --
 ALTER TABLE `profiles`
@@ -140,9 +302,29 @@ ALTER TABLE `profiles`
   ADD UNIQUE KEY `user_id` (`user_id`);
 
 --
+-- Indices de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `roles`
 --
 ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `taggables`
+--
+ALTER TABLE `taggables`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tag_type` (`tag_type`),
+  ADD KEY `tag_id` (`tag_id`);
+
+--
+-- Indices de la tabla `tags`
+--
+ALTER TABLE `tags`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -167,13 +349,25 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT de la tabla `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `models`
 --
 ALTER TABLE `models`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
@@ -182,10 +376,22 @@ ALTER TABLE `roles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT de la tabla `taggables`
+--
+ALTER TABLE `taggables`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+
+--
+-- AUTO_INCREMENT de la tabla `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
@@ -198,10 +404,23 @@ ALTER TABLE `images`
   ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`img_type`) REFERENCES `models` (`id`);
 
 --
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedores` (`id`);
+
+--
 -- Filtros para la tabla `profiles`
 --
 ALTER TABLE `profiles`
   ADD CONSTRAINT `profiles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `taggables`
+--
+ALTER TABLE `taggables`
+  ADD CONSTRAINT `taggables_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`),
+  ADD CONSTRAINT `taggables_ibfk_2` FOREIGN KEY (`tag_type`) REFERENCES `models` (`id`);
 
 --
 -- Filtros para la tabla `user_role`
