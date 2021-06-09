bae4d0f0cad7180e66f28408a4fa581efa1ec254
Alex <NaudisGarcia51@gmail.com>
2021-06-09 18:38:59 -0430

    cliente

--- controllers/ClienteController.php
+++ controllers/ClienteController.php
@@ -3,13 +3,12 @@
 class ClienteController  {
     
     public function __construct() {
-        require_once 'models/VentaModel.php';        
+        require_once 'models/ClienteModel.php';        
     }
 
     public function getById(){
-     
       $cedula = $_POST['Cedula-cliente'];
-      $venta = new VentaModel()  ;
+      $venta = new ClienteModel()  ;
       $data["cliente"] = $venta->getByCedula("cliente",  $cedula);
       
       if (!$data["cliente"] == NULL) {
@@ -17,57 +16,20 @@ class ClienteController  {
               'respuesta' => 'exito',
               'data' =>  $data["cliente"]              
             );
-           $data['url']= "GenerarVentas";
-
+           
       } else{
         $respuesta = array(
           'respuesta' => 'error',         
         );
-        $data['url']= "GenerarVentas2";
       }
-      
-      die(json_encode($respuesta));
-       
-      
-      
-     
+      die(json_encode($respuesta));     
     }
+  
 
-
-
-
-    
-    
-    public function Inset(){
-      $che= NULL;
-      $estatus="2";
-
-      if (isset($_POST['checkbox'])) {
-         $che = $_POST['checkbox'];
-      }
-      if (isset($_POST['estatus'])) {
-          $estatus = "1";
-       }
-      
-      $producto = new ProductoModel();
-      $dato = array(            
-          'nombre' =>  $_POST['nombre'],
-          'precio' => $_POST['precio'],
-          'marca' => $_POST['marca'],
-          'stock' => $_POST['stock'],
-          'descripcion' => $_POST['descripcion'],
-          'id_proveedor'=> $_POST['id_proveedor'],
-          
-          
-          'estatus' => $estatus
-      );
-      
-      
-
-      /*  var_dump($dato); */
-      $producto->Insert($dato ); 
-
-    }
+    public function Insert($dato){
+      $cliente = new ClienteModel();
+      $cliente->Insert($dato);   
+   }
 
     public function update(){
       echo "hola";
